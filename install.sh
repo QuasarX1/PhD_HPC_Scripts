43e96dad60af2e3fe1c8934dd14c15f4c54c3a92
Christopher Rowe <C.J.Rowe@2018.ljmu.ac.uk>
2023-03-04 18:00:00 +0000

    Fixes the installer.

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ scripts_directory=$(dirname "$(readlink -f ${BASH_SOURCE})")
 
 python -m venv environment
 source ./environment/bin/activate
+pip install --upgrade pip
+pip install numpy
 pip install -r $scripts_directory/requirements.txt
 deactivate
 
