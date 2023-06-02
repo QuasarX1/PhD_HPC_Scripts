3a9db4783419e8b42fd5a59cd9d0841c25cb6ede
Chris Rowe <aricrowe@login2.pri.prospero.alces.network>
2023-06-02 13:14:56 +0100

    Updates the installer to cope with no python alias to python3.

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 scripts_directory=$(dirname "$(readlink -f ${BASH_SOURCE})")
 
-python -m venv environment
+python3 -m venv environment
 source ./environment/bin/activate
+
 pip install --upgrade pip
 pip install numpy
+
 pip install -r $scripts_directory/requirements.txt
+
 deactivate
 
 echo "export QX1_PYTHON=$(readlink -f ./environment/bin)/python">$scripts_directory/.python_interpreter_path.sh
