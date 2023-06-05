ab2fa5ee70c6109c9ce2157b286cf1ce6155f2a9
Christopher Rowe <chris.rowe19@outlook.com>
2023-06-05 17:13:21 +0100

    Fixes the issues with instalation.

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ scripts_directory=$(dirname "$(readlink -f ${BASH_SOURCE})")
 python3 -m venv environment
 source ./environment/bin/activate
 
-pip install --upgrade pip
-pip install numpy
+#pip install --upgrade pip
+if [[ $(pip list | grep numpy | wc -l) -eq 0 ]]; then
+    pip install numpy
+fi
 
 pip install -r $scripts_directory/requirements.txt
 
