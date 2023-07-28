f7a3745cc6a1260f662da3b4dcef7f4a8577ba7d
Christopher Rowe <chris.rowe19@outlook.com>
2023-07-28 13:57:36 +0100

    Changes the commands for submitting jobs to allow the specifying of a partition.
    Both commands now require a -t flag to specify the wallclock time.

--- submit_pipline.sh
+++ submit_pipline.sh
@@ -5,14 +5,37 @@ scripts_directory=$(dirname "$(readlink -f ${BASH_SOURCE})")
 mkdir logs
 mkdir logs/errors
 
-if [ ! -z "${2}" ]
-then
-    time_flag="--time ${2} "
-else
-    time_flag=""
-fi
+# Process positional arguments
+cfg_file="${1}"
 
-jobid=$(sbatch $time_flag"$scripts_directory/data_pipeline.sh" "$scripts_directory" "$(readlink -f ${1})" | awk '{print $4}')
+# Process options
+shift 1
+
+time_flag=""
+partition=""
+
+while getopts "t:p:" opt
+do
+    case "$opt" in
+        t)
+            time_flag="--time $OPTARG "
+            ;;
+        n)
+            partition="--partition $OPTARG "
+            ;;
+        \?)
+            ;;
+    esac
+done
+
+#if [ ! -z "${2}" ]
+#then
+#    time_flag="--time ${2} "
+#else
+#    time_flag=""
+#fi
+
+jobid=$(sbatch $time_flag$partition"$scripts_directory/data_pipeline.sh" "$scripts_directory" "$(readlink -f $cfg_file)" | awk '{print $4}')
 
 echo "Use \`scancel $jobid\` to cancel."
 echo "Use \`watch tail \"$(readlink -f "logs/pipline-log-$jobid.txt")\" -n 40\` to view live output."
